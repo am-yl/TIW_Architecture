01ea9432402d095f93587591bb322d254fa454f5
am-yl <amyl@caramail.com>
2022-01-13 23:57:49 +0100

    changement padding btn + initialisation carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,15 +2,41 @@ let slider = document.querySelector('.carousel');
 if (slider) {
     let slides = slider.querySelectorAll('.slide');
     let nbSlide = slides.length;
+    let last = nbSlide-2;
+    let befLast = nbSlide-1;
     let showSlide = 0;
-    for (let i = 0; i < slides.length; i++) {
+    let i=0;
+
+    // on réinitialise le slider :
+
+    let delClass = ["left", "mid-left", "center", "mid-right", "right"]; // les classes d'affichages du slider
+    // on parcourt toutes les slides une fois et on retire les classes
+    while(i < nbSlide) {
+        slides[i].classList.remove(...delClass);
+        i++;
+    }
+    // on parcourt à nouveau (oui je sais) pour ajouter les classes sur 5 positions à la suite (n-2, n-1, 0, 1, 2 ; où n = nbSlide)
+    for (i = 0; i < nbSlide; i++) {
         let slide = slides[i];
-        if (i === 0) {
-            slide.classList.add('center');
-        } else {
-            slide.classList.remove('center');
+        switch (i) {
+            case 0 :
+                slide.classList.add('center');
+                break;
+            case 1 :
+                slide.classList.add('mid-right');
+                break;
+            case 2 :
+                slide.classList.add('right');
+                break;
+            case last :
+                slide.classList.add('left');
+                break;
+            case befLast :
+                slide.classList.add('mid-left');
+                break;    
         }
     }
+
     let prevArrow = slider.querySelector('.prev');
     let nextArrow = slider.querySelector('.next');
     prevArrow.addEventListener('click', function () {
@@ -28,6 +54,6 @@ if (slider) {
         if (showSlide >= nbSlide) {
             showSlide = 0;
         }
-        slides[showSlide].sclassList.add('center');
+        slides[showSlide].classList.add('center');
     });
 }
