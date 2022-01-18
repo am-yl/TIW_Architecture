3b9671129cd9d466a8089470a05caaf8d8c78fcc
am-yl <amyl@caramail.com>
2022-01-18 20:30:26 +0100

    modif var names & figcaptions shadow

--- assets/js/diaporama.js
+++ assets/js/diaporama.js
@@ -2,13 +2,12 @@ var slideIndex = 0;
 diaporama();
 
 function diaporama() {
-    var i;
-    var x = document.getElementsByClassName("diapo");
-    for (i = 0; i < x.length; i++) {
-        x[i].classList.remove('disImg');
+    let diapos = document.getElementsByClassName("diapo");
+    for (let i = 0; i < diapos.length; i++) {
+        diapos[i].classList.remove('disImg');
     }
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].classList.add('disImg');
+    if (slideIndex > diapos.length) {slideIndex = 1}
+    diapos[slideIndex-1].classList.add('disImg');
     setTimeout(diaporama, 6000); // Change image every 6 seconds
 }
